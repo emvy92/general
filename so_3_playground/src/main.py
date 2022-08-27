262e5c33e45f741970853b4ae0231a5424e35efb
emvy92 <mail@marvin-ahrens.de>
2022-08-27 19:06:35 +0200

    implement animation of intrinsic euler rotation

--- so_3_playground/src/main.py
+++ so_3_playground/src/main.py
@@ -1,3 +1,5 @@
+import rlcompleter
+from timeit import repeat
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from matplotlib.patches import FancyArrowPatch
@@ -10,7 +12,7 @@ class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
         FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
         self._verts3d = xs, ys, zs
-        self._rot = R.from_euler('zyx', [0.1, 0.0, 0.0], degrees=True)
+        self._rot = R.from_euler('ZYX', [0.1, 0.0, 0.0], degrees=True)
 
 
     def rotate(self, rot):
@@ -27,15 +29,32 @@ class Arrow3D(FancyArrowPatch):
 
 def update(frame):
     global yaw, pitch, roll
+    global progress_yaw, progress_pitch, progress_roll
     global arrowX, arrowY, arrowZ
-    global vecX, vecY, vecZ
-    yaw_end = 45.0
-    progress = yaw_end * frame/float(frames)
-    arrowX.rotate(R.from_euler('zyx', [progress,0.0,0.0], degrees=True))
-    return arrowX, 
+
+    third_of_frames=int(frames/3.0)
+    if frame < third_of_frames:
+        progress_yaw = yaw * float(frame+1)/third_of_frames
+    elif frame < (2 * third_of_frames):
+        progress_pitch = pitch * (float(frame+1)/third_of_frames-1.0)
+    else:
+        progress_roll = roll * (float(frame+1)/third_of_frames-2.0)
+    arrowX.rotate(R.from_euler('ZYX', [progress_yaw, progress_pitch, progress_roll], degrees=True))
+    arrowY.rotate(R.from_euler('ZYX', [progress_yaw, progress_pitch, progress_roll], degrees=True))
+    arrowZ.rotate(R.from_euler('ZYX', [progress_yaw, progress_pitch, progress_roll], degrees=True))
+    return (arrowX, arrowY, arrowZ) 
 
 
 def main():
+    global yaw, pitch, roll
+    yaw = 25.0
+    pitch = -45.0
+    roll = 45.0
+    global progress_yaw, progress_pitch, progress_roll
+    progress_yaw = 0.0
+    progress_pitch = 0.0
+    progress_roll = 0.0
+
     f = plt.figure(constrained_layout=True)
     f.suptitle("SO(3) Playground")
     gs = GridSpec(3, 3, figure=f)
@@ -51,26 +70,31 @@ def main():
     global arrowX
     global arrowY
     global arrowZ
-    global vecX
-    global vecY
-    global vecZ
-    global frames
-    frames = 300
-    vecX = np.array([1.0, 0.0, 0.0])
-    vecY = np.array([0.0, 1.0, 0.0])
-    vecZ = np.array([0.0, 0.0, 1.0])
-    arrowX = Arrow3D([0.0, vecX[0]], [0.0, vecX[1]], [0.0, vecX[2]],  mutation_scale=20, lw=3, arrowstyle="-|>", color="r")
-    arrowY = Arrow3D([0.0, vecY[0]], [0.0, vecY[1]], [0.0, vecY[2]],  mutation_scale=20, lw=3, arrowstyle="-|>", color="g")
-    arrowZ = Arrow3D([0.0, vecZ[0]], [0.0, vecZ[1]], [0.0, vecZ[2]],  mutation_scale=20, lw=3, arrowstyle="-|>", color="b")
+    arrowX = Arrow3D([0.0, 1.0], [0.0, 0.0], [0.0,  0.0],  mutation_scale=20, lw=3, arrowstyle="-|>", color="r")
+    arrowY = Arrow3D([0.0, 0.0], [0.0, 1.0], [0.0, 0.0],  mutation_scale=20, lw=3, arrowstyle="-|>", color="g")
+    arrowZ = Arrow3D([0.0, 0.0], [0.0, 0.0], [0.0, 1.0],  mutation_scale=20, lw=3, arrowstyle="-|>", color="b")
     ax.add_artist(arrowX)
     ax.add_artist(arrowY)
     ax.add_artist(arrowZ)
+
+    arrowTargetX = Arrow3D([0.0, 1.0], [0.0, 0.0], [0.0,  0.0],  mutation_scale=20, lw=3, arrowstyle="-|>", color="r", alpha=0.1)
+    arrowTargetX.rotate(R.from_euler('ZYX', [yaw, pitch, roll], degrees=True))
+    arrowTargetY = Arrow3D([0.0, 0.0], [0.0, 1.0], [0.0, 0.0],  mutation_scale=20, lw=3, arrowstyle="-|>", color="g", alpha=0.1)
+    arrowTargetY.rotate(R.from_euler('ZYX', [yaw, pitch, roll], degrees=True))
+    arrowTargetZ = Arrow3D([0.0, 0.0], [0.0, 0.0], [0.0, 1.0],  mutation_scale=20, lw=3, arrowstyle="-|>", color="b", alpha=0.1)
+    arrowTargetZ.rotate(R.from_euler('ZYX', [yaw, pitch, roll], degrees=True))
+    ax.add_artist(arrowTargetX)
+    ax.add_artist(arrowTargetY)
+    ax.add_artist(arrowTargetZ)
+
     ax.set_xlim(-1.0, 1.0)
     ax.set_ylim(-1.0, 1.0)
     ax.set_zlim(-1.0, 1.0)
     ax.axis('off')
 
-    anim = FuncAnimation(f, update, frames=frames, interval=10, blit=True)
+    global frames
+    frames = 300
+    anim = FuncAnimation(f, update, frames=frames, interval=30, blit=True, repeat=False)
 
     plt.show()
 
